e87c1e1126b2b10de039934870f6be45c2fb93f8
Miles Hoover <mhoover708@gmail.com>
2022-07-09 17:32:58 -0400

    initial commit

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+locals {
+    instance_name = "${terraform.workspace}-instance"
+}
+
+resource "aws_instance" "webserver" {
+    ami = var.ami
+    instance_type = var.instance_type
+
+    tags = {
+        Name = local.instance_name
+    }
+}
